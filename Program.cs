79aab075f2ffe94722fb4d94e287bac636b9ec5d
Styyyx <46678157+Styyyx@users.noreply.github.com>
2021-05-31 14:34:51 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieHub
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        internal static loginForm main;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            main = new loginForm();
+            Application.Run(main);
+        }
+    }
+}
